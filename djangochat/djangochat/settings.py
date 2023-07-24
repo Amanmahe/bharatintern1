2ac4eeb948c6bd0debcae660cc5c8df2681cb5b6
AMAN MAHESHWARI <98646204+Amanmahe@users.noreply.github.com>
2023-07-24 23:15:00 +0530

    a

--- djangochat/djangochat/settings.py
+++ djangochat/djangochat/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2wh%ua$nt&20)8sypf%f51j85@ly*wigme83rl4&5dg%^%&+^+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
